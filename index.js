461801412339dfbc761f51241b3a336c031b60d8
Shuvojit Tikader <shuvojittikader@gmail.com>
2025-08-29 03:37:31 +0600

    5rd commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let heartCount = 0;
 let coinCount = 100;
 let copyCount = 0;
-//
+
+// display element
 const heartDisplay = document.getElementById("heartCount");
-const coniDisplay = document.getElementById("coinCount");
+const coinDisplay = document.getElementById("coinCount");
 const copyDisplay = document.getElementById("copyCount");
 const historyList = document.getElementById("historyList");
+
 // heart button
 document.querySelectorAll(".heartBtn").forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -13,16 +15,17 @@ document.querySelectorAll(".heartBtn").forEach((btn) => {
     heartDisplay.textContent = heartCount;
   });
 });
+
 // copy Button
 document.querySelectorAll(".copyBtn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const hotline = e.target
-      .closest(".cart")
-      .queryselector(".hotline").textContent;
+      .closest(".card")
+      .querySelector(".hotline").textContent;
     navigator.clipboard.writeText(hotline);
-    alert(`Copid : ${hotline}`);
+    alert(`Copied : ${hotline}`);
     copyCount++;
-    coniDisplay.textContent = copyCount;
+    copyDisplay.textContent = copyCount;
   });
 });
 
@@ -30,28 +33,35 @@ document.querySelectorAll(".copyBtn").forEach((btn) => {
 document.querySelectorAll(".callBtn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const card = e.target.closest(".card");
-    const service = card.queryselector("h3").textcontent;
-    const hotline = card.queryselector("p.text-2xl").textcontent;
+    const service = card.querySelector("p.text-xl").textContent;
+    const hotline = card.querySelector(".hotline").textContent;
 
     if (coinCount < 20) {
       alert("Not enough coins to make this call!");
       return;
     }
+
     // cut coin
     coinCount -= 20;
-    document.getElementById("coinCount").textContent = coinCount;
+    coinDisplay.textContent = coinCount;
     alert(`Calling ${service} at ${hotline}`);
 
     // history
-    const now = new Date().toLocaleString();
-    const li = document.createComment("li");
-    li.className = "p-2 border-b";
-    li.textContent = `${service} - ${hotline} (at ${now})`;
+    const now = new Date().toLocaleTimeString();
+    const li = document.createElement("li");
+    li.className = "p-2 bg-gray-100 ";
+    li.innerHTML = `
+    <div class="flex justify-between">
+    <div>
+     <span class="text-2xl font-bold">${service}</span></br>
+    <span class=" text-xl font-bold">${hotline} </span></div>
+    <div><span>${now}</span></div>
+    </div>`;
     historyList.appendChild(li);
   });
 });
 
 // clear history
 document.getElementById("clearHistoryBtn").addEventListener("click", () => {
-  document.getElementById("historyList").innerHTML = "";
+  historyList.innerHTML = "";
 });
